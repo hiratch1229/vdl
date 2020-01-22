e8fd2eec59013ae25bf6dc7c025f35fa31eeda4a
hiratch1229 <hiratch981229@gmail.com>
2020-01-22 10:54:14 +0900

    ScenePostEffectバグ修正

--- vdlTest/Shader/PostEffect/BloomPS.hlsl
+++ vdlTest/Shader/PostEffect/BloomPS.hlsl
@@ -1,6 +1,7 @@
 #include "../Texture/Texture.hlsli"
 
 SamplerState Sampler : register(s0);
+SamplerState ClampSampler : register(s1);
 Texture2D Texture : register(t0);
 Texture2D ShrinkBuffer0 : register(t1);
 Texture2D ShrinkBuffer1 : register(t2);
@@ -9,12 +10,12 @@ Texture2D ShrinkBuffer3 : register(t4);
 
 float4 main(PS_IN In) : SV_TARGET
 {
-  float4 Color = Texture.Sample(Sampler, In.Texcoord);
+  float4 Color = Texture.Sample(ClampSampler, In.Texcoord);
 
-  Color += ShrinkBuffer0.Sample(Sampler, In.Texcoord);
-  Color += ShrinkBuffer1.Sample(Sampler, In.Texcoord);
-  Color += ShrinkBuffer2.Sample(Sampler, In.Texcoord);
-  Color += ShrinkBuffer3.Sample(Sampler, In.Texcoord);
+  Color += ShrinkBuffer0.Sample(ClampSampler, In.Texcoord);
+  Color += ShrinkBuffer1.Sample(ClampSampler, In.Texcoord);
+  Color += ShrinkBuffer2.Sample(ClampSampler, In.Texcoord);
+  Color += ShrinkBuffer3.Sample(ClampSampler, In.Texcoord);
 
   Color.a = 1.0f;
 
