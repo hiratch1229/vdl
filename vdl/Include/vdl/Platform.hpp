3400719d124d1c5fbfaec2c547eaf7152c9a9111
hiratch1229 <hiratch981229@gmail.com>
2023-06-04 03:18:25 +0900

    プロパティシートの整理

--- vdl/Include/vdl/Platform.hpp
+++ vdl/Include/vdl/Platform.hpp
@@ -13,29 +13,3 @@
 #elif defined VDL_USING_VULKAN
 #define VDL_EXECUTE_VULKAN
 #endif
-
-#if defined VDL_EXECUTE_DIRECTX11
-#if defined VDL_TARGET_WINDOWS
-#if defined _DEBUG
-#pragma comment(lib, "vdlForDirectX11OnWindows_d.lib")
-#else
-#pragma comment(lib, "vdlForDirectX11OnWindows.lib")
-#endif // _DEBUG
-#endif
-#elif defined VDL_EXECUTE_DIRECTX12
-#if defined VDL_TARGET_WINDOWS
-#if defined _DEBUG
-#pragma comment(lib, "vdlForDirectX12OnWindows_d.lib")
-#else
-#pragma comment(lib, "vdlForDirectX12OnWindows.lib")
-#endif // _DEBUG
-#endif
-#elif defined VDL_EXECUTE_VULKAN
-#if defined VDL_TARGET_WINDOWS
-#if defined _DEBUG
-#pragma comment(lib, "vdlForVulkanOnWindows_d.lib")
-#else
-#pragma comment(lib, "vdlForVulkanOnWindows.lib")
-#endif // _DEBUG
-#endif
-#endif
